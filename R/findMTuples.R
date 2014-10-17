75c954a2e38e355e9fd5dc726527cf8775f3b0c2
Peter Hickey <peter.hickey@gmail.com>
2014-10-17 11:09:57 +1100

    Re-wrote findMTuples. Using explicit loops improved performance. Still to investigate parallelisation and write unit tests.

--- R/findMTuples.R
+++ R/findMTuples.R
@@ -1,84 +1,89 @@
-# TODO: Function to find MTuples in a BSgenome
+# TODO: Should the example use library or require?
+# TODO: Unit tests. This could be difficult since the function requires a 
+# large input file (bsgenome) and takes a while to run; what's best practice?
 
+#' Find tuples of methylation loci of a given size in a reference genome.
+#' 
 #' @param bsgenome A \code{\link[BSgenome]{BSgenome}} object for the reference 
 #' genome of interest.
 #' @param methinfo A \code{\link{MethInfo}} object containing the type of 
 #' methylation loci for which to search.
 #' @param size An \code{integer} specifying the size of tuples for which to 
 #' search.
-#' @param ... Additional arguments. For example, 
-#' you may wish to only search a specific chromosome in the reference genome 
-#' via the \code{exclude} argument, which is passed to the 
-#' \code{\link[BSgenome]{BSParams}} initializer.
-findMTuples <- function(bsgenome, methinfo, size, ...) {
-  # 1. Argument checks
+#' @param exclude A character vector of \code{seqnames} to be filtered out from 
+#' the \code{bsgenome}.
+#' 
+#' @return An \code{\link{MTuples}} object containing the tuples.
+#' 
+#' @export
+#' 
+#' @examples
+#' \dontrun{
+#' # Find all 2-tuples of CG in the C. elegans reference genome
+#' library(BSgenome.Celegans.UCSC.ce2)
+#' findMTuples(bsgenome = Celegans, methinfo = MethInfo('CG'), size = 2)
+#' }
+findMTuples <- function(bsgenome, methinfo, size, exclude = NULL) {
+  # 0. Argument checks
+  if (!is(bsgenome, "BSgenome")) {
+    stop("'bsgenome' must be a 'BSgenome' object.")
+  }
+  if (!is(methinfo, "MethInfo")) {
+    stop("'methinfo' must be a 'MethInfo' object.")
+  }
+  size <- as.integer(size)
+  if (size < 1L) {
+    stop("'size' must be a positive integer.")
+  }
+  if (!is.character(exclude) && !is.null(exclude)) {
+    stop("'exclude' must be a character vector.")
+  }
+  
+  # 1. Variable initialisation
+  methtype <- methtype(methinfo)
+  seqinfo <- seqinfo(bsgenome)
+  seqnames <- seqnames(bsgenome)
+  seqnames <- seqnames[!seqnames %in% exclude]
+  list_of_mtuples <- vector(mode = "list", length = length(seqnames))
+  names(list_of_mtuples) <- seqnames
   
   # 2. Find all methylation loci with type given by methinfo
-  np <- length(methtype(methinfo))
-  pattern <- DNAStringSet(methtype(methinfo))
-  pattern <- c(pattern, reverseComplement(pattern))
-  strand <- rep(c('+', '-'), each = np)
-  names(pattern) <- strand
-  names(strand) <- strand
-  # TODO: Why doesn't bpmapply work?
-  # TODO: Use explicit loops and reverse order of loops, i.e. outer loop 
-  # chromosomes and inner loop patterns (see http://www.bioconductor.org/packages/release/bioc/vignettes/BSgenome/inst/doc/GenomeSearching.pdf)
-  loci <- mapply(FUN = function(bsgenome, pattern_, strand_) {
-    
-    bsparams <- new("BSParams", X = bsgenome, 
-                    FUN = function(pattern, subject, strand, ...) {
-                      if (strand == '+') {
-                        start(matchPattern(pattern, subject, fixed = FALSE, 
-                                           ...))
-                      } else if (strand == '-') {
-                        end(matchPattern(pattern, subject, fixed = FALSE, ...))
-                      }
-                    }, simplify = TRUE, ...)
-    bsapply(bsparams, pattern = pattern_, strand = strand_)
-  }, pattern_ = pattern, strand_ = strand, 
-  MoreArgs = list(bsgenome = bsgenome), SIMPLIFY = FALSE, ...)
-  # Now, combine methylation loci on a per-chromosome + per-strand basis.
-  # TODO: First, benchmark to see if it's worth fixing. It seems that there 
-  # must be a better way to do this, e.g. pre-allocating vectors or lapply 
-  # method. 
-  pps <- length(pattern) / 2
-  fwd_loci <- loci[[1]]
-  rev_loci <- loci[[1 + pps]]
-  if (pps > 1) {
-    for (i in seq.int(from = 2, to = pps, by = 1)) {
-      for (j in seq_along(loci[[i]])) {
-        fwd_loci[[j]] <- c(fwd_loci[[j]], loci[[i]][[j]])
-      }
+  # TODO: foreach would give instant parallelisation; can this be done via
+  # BiocParallel and is it worth it?
+  for (seqname in seqnames) {
+    subject <- bsgenome[[seqname]]
+    fwd_loci <- c()
+    rev_loci <- c()
+    for (mt in methtype) {
+      mt <- DNAString(mt)
+      rcmt <- reverseComplement(mt)
+      tmp_fwd_loci <- start(matchPattern(mt, subject, fixed = FALSE))
+      tmp_rev_loci <- end(matchPattern(rcmt, subject, fixed = FALSE))
+      fwd_loci <- c(fwd_loci, tmp_fwd_loci)
+      rev_loci <- c(rev_loci, tmp_rev_loci)
     }
-    for(i in seq.int(from = pps + 2, to = 2 * pps, by = 1)) {
-      for (j in seq_along(loci[[i]])) {
-        rev_loci[[j]] <- c(rev_loci[[j]], loci[[i]][[j]])
-      }
-    }
+    strand <- c(Rle('+', length(fwd_loci) - size + 1), 
+                Rle('-', length(rev_loci) - size + 1))
+    fwd_loci <- sort(fwd_loci)
+    rev_loci <- sort(rev_loci)
+    # TODO: Iterators/generators would be cool here rather than actually 
+    # creating the index variables.
+    fwd_idx <- sapply(seq_len(size), function(i, size, n) {
+      seq.int(i, n + i - size, 1)
+    }, size = size, n = length(fwd_loci))
+    rev_idx <- sapply(seq_len(size), function(i, size, n) {
+      seq.int(i, n + i - size, 1)
+    }, size = size, n = length(rev_loci))
+    fwd_tuples <- matrix(fwd_loci[fwd_idx], ncol = size)
+    rev_tuples <- matrix(rev_loci[rev_idx], ncol = size)
+    tuples <- rbind(fwd_tuples, rev_tuples)
+    list_of_mtuples[seqname] <- MTuples(gtuples = GTuples(seqnames = seqname, 
+                                                          tuples = tuples, 
+                                                          strand = strand, 
+                                                          seqinfo = seqinfo),
+                                        methinfo = methinfo)
   }
-  fwd_loci <- lapply(fwd_loci, sort)
-  rev_loci <- lapply(rev_loci, sort)
-  
-  # 3. Create all m-tuples of those loci
   # TODO: Is there a better way to create a single MTuples object than 
   # unlist(MTuplesList(list_of_MTuples))?
-  mt <- bpmapply(function(seqname, fwd_loci_, rev_loci_, m, methinfo, seqinfo) {
-    strand <- c(Rle('+', length(fwd_loci_) - m + 1), 
-                Rle('-', length(rev_loci_) - m + 1))
-    # TODO: Generators would be cool here
-    fwd_idx <- sapply(seq_len(m), function(i, m, n) {
-      seq.int(i, n + i - m, 1)
-    }, m = m, n = length(fwd_loci_))
-    rev_idx <- sapply(seq_len(m), function(i, m, n) {
-      seq.int(i, n + i - m, 1)
-    }, m = m, n = length(rev_loci_))
-    fwd_tuples <- matrix(fwd_loci_[fwd_idx], ncol = m)
-    rev_tuples <- matrix(rev_loci_[rev_idx], ncol = m)
-    MTuples(GTuples(seqnames = seqname, tuples = rbind(fwd_tuples, rev_tuples), 
-                    strand = strand, seqinfo = seqinfo), methinfo = methinfo)
-  }, seqname = names(fwd_loci), fwd_loci_ = fwd_loci, 
-  rev_loci_ = rev_loci, 
-  MoreArgs = list(m = m, methinfo = methinfo, seqinfo = seqinfo(bsgenome)), 
-  SIMPLIFY = FALSE)
-  unlist(MTuplesList(mt), use.names = FALSE)
+  unlist(MTuplesList(list_of_mtuples), use.names = FALSE)
 }
